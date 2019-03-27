1f0203bdabfffbcd267b0c8842a78bf72c4e518a
Daniel Haro <dannharo@gmail.com>
2019-03-27 10:53:41 -0600

    solved more test
    	modified:   app/bestPractices.js
    	modified:   app/count.js
    	modified:   app/flowControl.js
    	modified:   app/logicalOperators.js
    	modified:   app/modules.js
    	modified:   app/numbers.js
    	modified:   app/strings.js

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,51 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    function pad(number, length) {
+      var str = '' + number;
+      while (str.length < length) {
+        str = '0' + str;
+      }
+      return str;
+    }
+    var numBin = Math.abs(num).toString(2);
+    pad(numBin, 8);
+    return parseInt(numBin.split('').reverse().join('').charAt(bit-1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    function pad(number, length) {
+      var str = '' + number;
+      while (str.length < length) {
+        str = '0' + str;
+      }
+      return str;
+    }
+    return pad(Math.abs(num).toString(2), 8);
   },
 
   multiply: function(a, b) {
+    function adjust(num) {
+      var exponent, multiplier;
 
+      if (num < 1) {
+        exponent = Math.floor( Math.log(num) * -1 );
+        multiplier = Math.pow(10, exponent);
+
+        return {adjusted: num * multiplier, multiplier: multiplier};
+      }
+
+      return {adjusted: num, multiplier: 1};
+    }
+    a = adjust(a);
+    b = adjust(b);
+
+    var result = (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
+
+    return result;
   }
 };
